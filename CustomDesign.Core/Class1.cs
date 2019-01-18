676f9990eee05f5c1481c9daf39d4135327e06cb
piorosen <aoikazto@gmail.com>
2019-01-18 13:11:36 +0900

    a

---
+++ CustomDesign.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CustomDesign.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ CustomDesign.CuiTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CustomDesign.CUITest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
