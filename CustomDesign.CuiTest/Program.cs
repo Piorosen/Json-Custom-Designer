9481d9a6a65153994c4f24b1d56ad6322a5c7960
piorosen <aoikazto@gmail.com>
2019-01-18 14:08:41 +0900

    need check CustomType File

--- CustomDesign.CuiTest/Program.cs
+++ CustomDesign.CuiTest/Program.cs
@@ -1,12 +1,57 @@
 ﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CustomDesign.CUITest
 {
     class Program
     {
+        class testlib
+        {
+            
+
+            private int Value = 0;
+            private Point Pt = new Point();
+
+            private int _property = 0;
+            private int Property
+            {
+                get
+                {
+                    return _property;
+                }
+                set
+                {
+                    _property = value;
+                }
+            }
+
+            private Rectangle rectangle = new Rectangle();
+
+
+            void CallFunc()
+            {
+
+                Console.WriteLine("Call Func");
+            }
+
+            void CallFunc(int data, Point pt)
+            {
+                Console.WriteLine($"{data} : {pt.X}, {pt.Y}");
+            }
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> list = new List<string>();
+            list.Add("123");
+            var t = list;
+            t[0] = "333";
+            Console.WriteLine(list[0]);
+            list.ToList();
+
+
         }
     }
 }
